b80dd19f85f1c031c0aa8f16621338388ff63103
ppeb <ppeb@ppeb.me>
2024-11-29 21:23:11 -0600

    Add :ProjectRemoveNugetDep and :ProjectAddNugetDep

--- lua/solution/commands.lua
+++ lua/solution/commands.lua
@@ -125,6 +125,83 @@ local commands = {
         name = "SolutionLog",
         func = function(_) vim.cmd(string.format("tabnew %s", log.log_path)) end,
     },
+    {
+        name = "ProjectAddNugetDep",
+        func = function(opts)
+            local ppn = assert(opts.fargs[1], "A project name or path must be passed as argument 1")
+            local project =
+                assert(utils.resolve_project(ppn), string.format("Project '%s' was found in the solution!", ppn))
+
+            local package_name = assert(opts.fargs[2], "A package name must be provided as argument 2")
+            local package_ver = opts.fargs[3]
+
+            project:add_nuget_dep(package_name, package_ver, function(success, message, code)
+                if not success then
+                    print(
+                        string.format(
+                            "Failed to add package '%s' to project '%s'%s%s",
+                            package_name,
+                            project.name,
+                            (message and ", " .. message) or "",
+                            (code and ", code: " .. code) or ""
+                        )
+                    )
+                else
+                    print(string.format("Successfully added package '%s' to project '%s'", package_name, project.name))
+                end
+            end)
+        end,
+        opts = {
+            nargs = "+",
+            complete = function(arg_lead, cmd_line, cursor_pos)
+                return utils.complete_2args(arg_lead, cmd_line, cursor_pos, function()
+                    return vim.tbl_map(function(e) return e.name end, _aggregate_projects)
+                end, function(_)
+                    -- TODO: Complete nuget packages using ???
+                    return {}
+                end)
+            end,
+        },
+        cond = function() return vim.tbl_count(_aggregate_projects) ~= 0 end,
+    },
+    {
+        name = "ProjectRemoveNugetDep",
+        func = function(opts)
+            local ppn = assert(opts.fargs[1], "A project name or path must be passed as argument 1")
+            local project =
+                assert(utils.resolve_project(ppn), string.format("Project '%s' was found in the solution!", ppn))
+
+            local package_name = assert(opts.fargs[2], "A package name must be provided as argument 2")
+
+            project:remove_nuget_dep(package_name, function(success, message, code)
+                if not success then
+                    print(
+                        string.format(
+                            "Failed to remove package '%s' from project '%s'%s%s",
+                            package_name,
+                            project.name,
+                            (message and ", " .. message) or "",
+                            (code and ", code: " .. code) or ""
+                        )
+                    )
+                else
+                    print(string.format("Successfully added package '%s' to project '%s'", package_name, project.name))
+                end
+            end)
+        end,
+        opts = {
+            nargs = "+",
+            complete = function(arg_lead, cmd_line, cursor_pos)
+                return utils.complete_2args(arg_lead, cmd_line, cursor_pos, function()
+                    return vim.tbl_map(function(e) return e.name end, _aggregate_projects)
+                end, function(_)
+                    -- TODO: Complete nuget packages using ???
+                    return {}
+                end)
+            end,
+        },
+        cond = function() return vim.tbl_count(_aggregate_projects) ~= 0 end,
+    },
 }
 
 --- @param slns SolutionFile[]
