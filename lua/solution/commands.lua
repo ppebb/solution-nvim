fa241d309dc39bd885ddd84795f600e756f9afe2
ppeb <ppeb@ppeb.me>
2024-12-01 00:29:38 -0600

    Add :ProjectRemoveDep in place of specific removal commands
    This commit replaces :ProjectRemoveNugetDep with :ProjectRemoveDep,
    which can take a dependency name and remove any of them, identifying the
    correct function to call on the project. This is (imo) better design
    than making the user pick which type of dependency to use.

--- lua/solution/commands.lua
+++ lua/solution/commands.lua
@@ -186,37 +186,65 @@ local commands = {
         cond = function() return vim.tbl_count(_aggregate_projects) ~= 0 end,
     },
     {
-        name = "ProjectRemoveNugetDep",
+        name = "ProjectRemoveDep",
         func = function(opts)
             local ppn = assert(opts.fargs[1], "A project name or path must be passed as argument 1")
             local project =
                 assert(utils.resolve_project(ppn), string.format("Project '%s' was found in the solution!", ppn))
 
-            local package_name = assert(opts.fargs[2], "A package name must be provided as argument 2")
+            local dep_name = assert(opts.fargs[2], "A dependency name must be provided as argument 2")
+            local dep =
+                assert(project:dependency_from_name(dep_name), "A valid dependency name must be provided as argument 2")
 
-            project:remove_nuget_dep(package_name, function(success, message, code)
+            local function response_handler(success, message, code)
                 if not success then
                     print(
                         string.format(
-                            "Failed to remove package '%s' from project '%s'%s%s",
-                            package_name,
+                            "Failed to remove dependency '%s' of type '%s' from project '%s'%s%s",
+                            dep.name,
+                            dep.type,
                             project.name,
                             (message and ", " .. message) or "",
                             (code and ", code: " .. code) or ""
                         )
                     )
                 else
-                    print(string.format("Successfully added package '%s' to project '%s'", package_name, project.name))
+                    print(
+                        string.format("Successfully removed dependency '%s' from project '%s'", dep.name, project.name)
+                    )
                 end
-            end)
+            end
+
+            if dep.type == "Nuget" then
+                project:remove_nuget_dep(dep.package, response_handler)
+            elseif dep.type == "Project" then
+                project:remove_project_ref(dep.project, response_handler)
+            elseif dep.type == "Local" then
+                project:remove_local_dep(dep.path, response_handler)
+            else
+                -- This should never hit, or something has gone very wrong
+                error(
+                    string.format(
+                        "Attempted to remove dependency of unkown type '%s' from project '%s'",
+                        dep.type,
+                        project.name
+                    )
+                )
+            end
         end,
         opts = {
             nargs = "+",
             complete = function(arg_lead, cmd_line, cursor_pos)
                 return utils.complete_2args(arg_lead, cmd_line, cursor_pos, function()
                     return vim.tbl_map(function(e) return e.name end, _aggregate_projects)
-                end, function(_)
-                    -- TODO: Complete nuget packages using ???
+                end, function(arg1)
+                    local project = utils.resolve_project(arg1)
+                    if project then
+                        return {
+                            vim.tbl_map(function(e) return e.name end, project.dependencies),
+                        }
+                    end
+
                     return {}
                 end)
             end,
