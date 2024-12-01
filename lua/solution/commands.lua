c8096ceb05a5254cefee69991b5f26210b17c76f
ppeb <ppeb@ppeb.me>
2024-12-01 00:45:14 -0600

    Add :ProjectAddLocalDep

--- lua/solution/commands.lua
+++ lua/solution/commands.lua
@@ -186,6 +186,27 @@ local commands = {
         cond = function() return vim.tbl_count(_aggregate_projects) ~= 0 end,
     },
     {
+        name = "ProjectAddLocalDep",
+        func = function(opts)
+            local ppn = assert(opts.fargs[1], "A project name or path must be passed as argument 1")
+            local project = assert(utils.resolve_project(ppn), string.format("Project '%s' could not be found!", ppn))
+
+            local dep_path = assert(opts.fargs[2], "A dll path must be provided as argument 2")
+            assert(dep_path:find("%.dll$"), "A dll path must be provided as argument 2")
+
+            local success, err = project:add_local_dep(dep_path)
+            if not success then
+                error(string.format("Failed to add dependency '%s' to project '%s', %s", dep_path, project.name, err))
+            else
+                print(string.format("Successfully added dependency '%s' to project '%s'", dep_path, project.name))
+            end
+        end,
+        opts = {
+            nargs = 1,
+            complete = "file",
+        },
+    },
+    {
         name = "ProjectRemoveDep",
         func = function(opts)
             local ppn = assert(opts.fargs[1], "A project name or path must be passed as argument 1")
