7149f026bebe84d4847550a90d39cc6781aab29d
ppeb <ppeb@ppeb.me>
2024-12-01 00:26:00 -0600

    Add :SolutionRegister
    :SolutionRegister allows one to provide a project or solution path and
    force the plugin to pick it up in the event it isn't autodetected. This
    mainly serves to improve autocomplete of commands and eventually for it
    to show in the UI without having to first be picked up in some
    roundabout method of getting the plugin to recognize it.

--- lua/solution/commands.lua
+++ lua/solution/commands.lua
@@ -18,6 +18,27 @@ local _aggregate_projects
 --- @type Command[]
 local commands = {
     {
+        name = "SolutionRegister",
+        func = function(opts)
+            local fname = assert(opts.fargs[1], "A solution or project file must be provided as argument 1")
+            local csproj = fname:find("%.csproj$")
+            local sln = fname:find("%.sln$")
+            assert(csproj or sln, "A solution or project file must be provided as argument 1")
+
+            assert(utils.file_exists(fname), string.format("The file '%s' does not exist!", fname))
+
+            if csproj then
+                require("solution.projectfile").new_from_file(fname)
+            else
+                table.insert(require("solution").slns, require("solution.solutionfile").new(fname))
+            end
+        end,
+        opts = {
+            nargs = 1,
+            complete = "file",
+        },
+    },
+    {
         name = "SolutionRemoveProject",
         func = function(opts)
             local sln_name = assert(opts.fargs[1], "A solution name must be provided as argument 1")
